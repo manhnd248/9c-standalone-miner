0593a787d2ac5713f503d4a12c86131043c4d8ca
Blaque Allen <CryptoKasm>
2021-01-16 23:28:11 -0600

    Added docker file choices

--- 9c-standalone-miner.sh
+++ 9c-standalone-miner.sh
@@ -17,25 +17,35 @@ fi
 # docker-compose
 
 # Update: Repository
-# git sync https://www.github.com/CryptoKasmDev/9c-standalone-miner.git 
+echo "Updating repository..."
+git pull https://www.github.com/CryptoKasmDev/9c-standalone-miner.git 
 
 
 # Update: Snapshot
-#sh ./linux-scripts/refresh-snapshot.sh $NC_CONTAINERS
+sh ./linux-scripts/refresh-snapshot.sh $NC_CONTAINERS
 
-echo $NC_SECRET_KEY
-echo $NC_CONTAINERS
-echo $NC_AUTORESTART
-echo $NC_AUTORESTARTTIMER
-echo $NC_LOGGING
-echo $NC_DISPLAYLOG
-
+echo "Starting docker containers..."
 # Start: Docker Containers
-
+if [[ NC_VOLUMES == "1" ]]  then
+  docker-compose -f docker/docker-compose.1.yml up -d
+elif [[ NC_VOLUMES == "2" ]]    then
+  docker-compose -f docker/docker-compose.2.yml up -d
+elif [[ NC_VOLUMES == "3" ]]    then
+  docker-compose -f docker/docker-compose.3.yml up -d
+elif [[ NC_VOLUMES == "4" ]]    then
+  docker-compose -f docker/docker-compose.4.yml up -d
+elif [[ NC_VOLUMES == "5" ]]    then
+  docker-compose -f docker/docker-compose.5.yml up -d
+elif [[ NC_VOLUMES == "6" ]]    then
+  docker-compose -f docker/docker-compose.6.yml up -d
+else
+  docker-compose -f docker/docker-compose.1.yml up -d
+fi
 
 
 # Display status of all containers
-
+echo "See the full log: `docker-compose logs --tail=100 -f` "
+echo "Watch for mined blocks: `docker-compose logs --tail=100 -f | grep -A 10 --color -i 'Mined a block'`" 
 
 # IF DisplayLog=True, auto run styilzed trail on containers
 
@@ -46,4 +56,4 @@ echo "  Github - https://github.com/CryptoKasm"
 echo "  Support - https://discord.gg/k6z2GS4yh2"
 echo "--------------------------------------------\n"
 
-read -p "Press any key to exit..."
+read -p "  Press any key to exit..."
